08f930f51537005618119e51668649eed0cbab0a
Bgame <bgamex@outlook.com>
2026-08-17 22:11:13 -0400

    Initial commit

---
+++ src/main/java/dev/dcemi.java
@@ -0,0 +1,21 @@
+package dev;
+
+import dev.command.ConsoleCommandHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("dcemi")
+public class dcemi {
+
+    public static final String MOD_ID = "dcemi";
+
+    public dcemi() {
+        FMLJavaModLoadingContext.get().getModEventBus()
+                .addListener(this::clientside);
+    }
+
+    private void clientside(FMLClientSetupEvent event) {
+        ConsoleCommandHandler.start();
+    }
+}
